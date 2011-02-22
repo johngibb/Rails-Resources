75ab290eeff7d8b93074482667b71bece123ac81
John Gibb <johngibb@gmail.com>
2011-02-21 18:06:44 -0800

    link section headers

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,6 +1,6 @@
 module HomeHelper
-  def section(header, options={}, &block)
-    options.merge! :header => header, :body => capture(&block)
+  def section(header, link, options={}, &block)
+    options.merge! :header => header, :link => link, :body => capture(&block)
     
     render :shared => :section, :locals => options
   end

19e2ce3602310d107d02f41e7604b053c153c525
John Gibb <johngibb@gmail.com>
2011-02-22 13:58:23 -0800

    pretty formatting in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,7 @@ RailsResources::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  Slim::Engine.set_default_options :pretty => true
 end
 

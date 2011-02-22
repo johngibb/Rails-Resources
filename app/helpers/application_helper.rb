a51cad3a740bd3b3a766b29fa83bb16b82327bf0
John Gibb <johngibb@gmail.com>
2011-02-21 20:39:54 -0800

    move more things to partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,15 @@ module ApplicationHelper
     link_to title, url, :class => 'bare'
   end
   
+  def render_collection(items, options={})
+    f = items.first
+    if f
+      plural = f.class.to_s.underscore.pluralize
+      local = options[:as] || plural.to_sym
+      render :partial => "#{plural}/#{plural}", :locals => { local => items }
+    end
+  end
+  
   def render(*args)
     options = args.first
     if options.is_a? Hash and options.key? :shared

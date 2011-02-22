6ace397d2d94d5c885593b8c8fcc1e2524057b6f
John Gibb <johngibb@gmail.com>
2011-02-21 17:54:05 -0800

    refactor redundant section markup into partial, and make a helper to use it

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,7 @@
 module HomeHelper
+  def section(header, options={}, &block)
+    options.merge! :header => header, :body => capture(&block)
+    
+    render :shared => :section, :locals => options
+  end
 end

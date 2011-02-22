e76608b892275224246797fa9241c75f5020cb0a
John Gibb <johngibb@gmail.com>
2011-02-21 16:14:36 -0800

    Populate seeds with existing file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+StackOverflowSearch.create :title => "Rails", :prefix => "[ruby-on-rails]"
+StackOverflowSearch.create :title => "Ruby", :prefix => "[ruby]"
+StackOverflowSearch.create :title => "Active Record", :prefix => "[activerecord]"
+
+Link.create :title => "Rails API", :url => "http://api.rubyonrails.org/"
+Link.create :title => "Migrations", :url => "http://api.rubyonrails.org/classes/ActiveRecord/Migration.html"
+Link.create :title => "Paperclip", :url => "http://thewebfellas.com/blog/2008/11/2/goodbye-attachment_fu-hello-paperclip"
+Link.create :title => "Observer", :url => "http://stackoverflow.com/questions/2242173/how-do-i-create-a-facebook-style-news-feed-in-ruby-on-rails/2247395#2247395"
+
+Snippet.create :title => "ActiveRecord SQL to STDOUT", :content => <<-END
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+END

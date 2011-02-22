a218a1ee9d2ffc21e963869d947b0c1434161e9d
John Gibb <johngibb@gmail.com>
2011-02-21 17:53:18 -0800

    Add support for render :shared => 'view'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  
+  def render(*args)
+    options = args.first
+    if options.is_a? Hash and options.key? :shared
+      options.merge! :partial => shared_partial_path(options.delete(:shared))
+    end
+    super *args
+  end
+  
+  private
+  
+  def shared_partial_path(partial_name)
+    "shared/#{partial_name}"
+  end
 end

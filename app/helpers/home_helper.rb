5cec13e4d5f90f7e8a30ffc9cd7b37a2a0046c9a
John Gibb <johngibb@gmail.com>
2011-02-22 13:34:50 -0800

    Simplify section code, and add a link to create a new object

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,28 @@
 module HomeHelper
-  def section(header, link, options={}, &block)
-    options.merge! :header => header, :link => link, :body => capture(&block)
-    
-    render :shared => :section, :locals => options
+  
+  def section(model_class, options={}, &block)
+    locals = { :title    => model_class.to_s.pluralize, 
+               :link     => index_path(model_class),
+               :new_link => new_path(model_class),
+               :body     => capture(&block)
+             }
+             
+    locals.merge! options
+
+    render :shared => :section, :locals => locals
+  end
+  
+  private
+  
+  def index_path(model_class)
+    plural = model_class.to_s.underscore.pluralize
+    function = "#{plural}_path"
+    send function.to_sym
+  end
+  
+  def new_path(model_class)
+    singular = model_class.to_s.underscore
+    function = "new_#{singular}_path"
+    send function.to_sym
   end
 end

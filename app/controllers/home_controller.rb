5d5b1b0183b64a9ed50317b8eceb566062c5f07b
John Gibb <johngibb@gmail.com>
2011-04-18 20:04:01 -0700

    add whatismyip functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
+  skip_before_filter :verify_admin, :only => :myip
+  
   def index
     @searches = StackOverflowSearch.all
     @snippets = Snippet.all
     @links = Link.all
+  end
+  
+  def myip
+    render :text => request.env["REMOTE_ADDR"]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ RailsResources::Application.routes.draw do
   resources :stack_overflow_searches
 
   root :to => "home#index"
+  
+  get '/myip' => "home#myip"
 end
